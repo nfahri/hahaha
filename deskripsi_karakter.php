87cbdff5b0a35b84fd3493e16dc295d7a4ab8c66
aghagha <amrikudou@gmail.com>
2015-05-28 18:52:50 +0700

    test

--- deskripsi_karakter.php
+++ deskripsi_karakter.php
@@ -15,48 +15,50 @@
 				echo "
 				<div class=\"row kotak-status\">
 					<!-- nama -->
-					<div class=\"col-sm-3\">
-						
-					</div>
-					<div class=\"col-sm-3\">
+					<div class=\"col-sm-4\"></div>
+					<div class=\"col-sm-1\">
 						<img src=\"images/char.png\" class=\"gambar\">
 					</div>
-					<div class=\"col-sm-6 status\">
+					<div class=\"col-sm-3 status text-center\">
 						".$deskripsi_karakter["karakter_nama"]."
 					</div>
+					<div class=\"col-sm-4 status\"></div>
 
 					<!-- HP -->
-					<div class=\"col-sm-1\"></div>
-					<div class=\"col-sm-2\">
+					<div class=\"col-sm-4\"></div>
+					<div class=\"col-sm-1\">
 						<img src=\"images/HP.png\" class=\"gambar\">
 					</div>
-					<div class=\"col-sm-3 status\">
+					<div class=\"col-sm-1 status\">
 						".$deskripsi_karakter["karakter_hp"]."
 					</div>
+
 					<!-- ATK -->
-					<div class=\"col-sm-1\"></div>
-					<div class=\"col-sm-2\">
+					<div class=\"col-sm-1\">
 						<img src=\"images/attack.png\" class=\"gambar\">
 					</div>
-					<div class=\"col-sm-3 status\">
+					<div class=\"col-sm-4 status\">
 						".$deskripsi_karakter["karakter_atk"]."
 					</div>
-					<!-- DEF -->
 					<div class=\"col-sm-1\"></div>
-					<div class=\"col-sm-2\">
+
+					<!-- DEF -->
+					<div class=\"col-sm-4\"></div>
+					<div class=\"col-sm-1\">
 						<img src=\"images/defense.png\" class=\"gambar\">
 					</div>
-					<div class=\"col-sm-3 status\">
+					<div class=\"col-sm-1 status\">
 						".$deskripsi_karakter["karakter_def"]."
 					</div>
+
 					<!-- EVA -->
-					<div class=\"col-sm-1\"></div>
-					<div class=\"col-sm-2\">
+					<div class=\"col-sm-1\">
 						<img src=\"images/evasion.png\" class=\"gambar\">
 					</div>
-					<div class=\"col-sm-3 status\">
+					<div class=\"col-sm-4 status\">
 						".$deskripsi_karakter["karakter_eva"]."
 					</div>
+					<div class=\"col-sm-1\"></div>
 				</div>
 				<div class=\"row\">
 					<div class=\"col-sm-4 status\"></div>
